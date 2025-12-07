34c2e954351f641e0a4d572893929e6960685299
Jorell Relleve <jorellrelleve@gmail.com>
2025-12-07 20:28:45 +0800

    feat: add new dep and do some adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
   	extend: {
+  	  fontFamily: {
+  	    inter: ["Inter", "sans-serif"],
+  	  }, 
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
