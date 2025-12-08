fe7430248452602bf3215a9c3ebc84dab0b92822
Jorell Relleve <jorellrelleve@gmail.com>
2025-12-08 20:14:02 +0800

    feat: add text input for generating quiz

--- src/components/custom/GenerateQuiz.tsx
+++ src/components/custom/GenerateQuiz.tsx
@@ -1,13 +1,25 @@
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { ArrowUp } from 'lucide-react'
+
 const GenerateQuiz = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center p-5 text-center">
-    <div className="flex flex-col w-full gap-y-3">
-    <span className="text-3xl font-extrabold font-inter">
-      Generate Interactive Quizzes with the Power of AI
-    </span>
-    <span className="text-slate-500">
-     Choose your difficulty, select a quiz type, and generate instantly
-    </span>
+    <div className="w-full min-h-screen flex flex-col md:items-center md:justify-center p-5 text-center">
+    <div className="flex flex-1 md:flex-none flex-col w-full md:max-w-80 gap-y-3 relative">
+      <span className="text-3xl font-extrabold font-inter">
+       Generate Interactive Quizzes with the Power of AI
+      </span>
+      <span className="text-slate-500">
+        Choose your difficulty, select a quiz type, and generate instantly
+      </span>
+      <div className="w-full flex-1 absolute md:relative bottom-16">
+            <div className="w-full relative md:mt-48">
+        <Input placeholder="Enter your desired quiz" className="py-6 rounded-lg"/>  
+        <Button className="absolute right-2 bottom-2 p-3">
+        <ArrowUp />
+        </Button>
+      </div> 
+      </div>
     </div>
     </div>
    )
