79df6e0859ee468ff4c3568e4cbff394b82a4d1b
Jorell Relleve <jorellrelleve@gmail.com>
2025-12-09 14:34:20 +0800

    feat: make a new reusable custom dropdown, add its functions,and remove the old ones

---
+++ src/components/custom/CustomDropdown.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button"
+import { ChevronDown, Check } from 'lucide-react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuLabel, 
+  DropdownMenuSeparator, 
+} from "@/components/ui/dropdown-menu"
+import type { Dispatch, SetStateAction } from 'react';
+import { useMemo, useCallback, memo } from 'react';
+import type { QuizData } from '@/components/custom/GenerateQuiz';
+
+interface ICustomDropdown {
+  title: string;
+  options: string[];
+  state: QuizData;
+  setState: Dispatch<SetStateAction<QuizData>>;
+}
+
+const CustomDropdown = (props: ICustomDropdown) => {
+  const { title, options, state, setState } = props;
+  const checkedOption: string = useMemo(() => state[(title === "quizType" ? title : title?.toLowerCase()) as keyof QuizData], [state, title])
+  
+  const selectOption = useCallback((e: Event) => {
+    e.preventDefault();
+    const id = (e.currentTarget as HTMLElement).id;
+    setState((prev: QuizData) => ({
+     ...prev,
+     [title]: id
+    }))
+    console.log(state)
+  }, [setState, checkedOption, state]);
+  
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="font-inter capitalize">
+        {checkedOption} 
+        <ChevronDown />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40 font-inter" align="start">
+        <DropdownMenuLabel className="capitalize" >{title === "quizType"? "Quiz Type" : title}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          {options?.map((option: string) => (<DropdownMenuItem onSelect={selectOption} id={option} key={option} className="capitalize">{option}
+          {checkedOption?.toLowerCase() === option && <Check className="ml-auto"/>} </DropdownMenuItem>))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default memo(CustomDropdown);
--- src/components/custom/GenerateQuiz.tsx
+++ src/components/custom/GenerateQuiz.tsx
@@ -1,10 +1,23 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { ArrowUp } from 'lucide-react'
-import Difficulty from '@/components/custom/Difficulty'
-import QuizType from '@/components/custom/QuizType'
+import CustomDropdown from '@/components/custom/CustomDropdown'
+import { useState } from 'react';
+
+export interface QuizData {
+  difficulty: string;
+  quizType: string;
+}
+
+const difficultyOptions: string[] = ["beginner", "intermediate", "advanced", "expert"]
+const quizTypeOptions: string[] = ["multiple choice", "identification", "enumeration", "mixed"]
 
 const GenerateQuiz = () => {
+  const [quizData, setQuizData] = useState<QuizData>({
+    difficulty: "beginner", 
+    quizType: "multiple choice", 
+  });
+  
   return (
     <div className="w-full min-h-screen flex flex-col md:items-center md:justify-center p-5 text-center">
     <div className="flex flex-1 md:flex-none flex-col w-full md:max-w-80 gap-y-3 relative">
@@ -16,8 +29,8 @@ const GenerateQuiz = () => {
       </span>
       <div className="w-full flex-1 absolute md:relative bottom-16  md:mt-48 flex flex-col gap-y-2">
         <div className="flex gap-x-2">
-          <Difficulty /> 
-          <QuizType />
+          <CustomDropdown title="difficulty" options={difficultyOptions} state={quizData} setState={setQuizData} /> 
+          <CustomDropdown title="quizType" options={quizTypeOptions} state={quizData} setState={setQuizData} /> 
         </div>
         <div className="w-full relative">
         <Input placeholder="Enter your desired quiz" className="py-6 rounded-lg"/>  
